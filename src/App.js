a72dffe05c35c1c12a5a7c86d43724122b64c4bd
Lis Phelps <79932328+lisphelps@users.noreply.github.com>
2021-04-27 12:59:41 -0500

    File clean up and roughing out in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ClassName="left">
+        <h1>Learn to code by watching others</h1>
+          <p>See how experienced developers solve problems in real-time. watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
+      <div ClassName="right">
+        <div ClassName="tryit">
+          <p><em>Try it free for 7 days</em> then $20/mo. thereafer</p>
+        </div>
+        <div ClassName="formcontainer">
+        <form>
+        <input id="first"></input>
+        <input id="last"></input>
+        <input id="email"></input>
+        <input id="password"></input>
+        <submit><button>CLAIM YOUR FREE TRIAL</button></submit>
+        </form>
+        <span ClassName="disclaimer">By clicking the button, you are agreeing to our <a href="index.html">Terms and Services</a></span>
+        </div>
+      </div>
     </div>
   );
 }
