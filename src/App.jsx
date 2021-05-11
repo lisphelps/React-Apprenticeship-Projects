df54b34a00e48747ee097cfed7d415fc3fc06831
Lis Phelps <79932328+lisphelps@users.noreply.github.com>
2021-05-11 08:23:57 -0500

    File reorganizing - preparing for additional pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css';
 import React from 'react';
-import Left from './components/Left/Left';
-import Right from './components/Right/Right';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Left />
-      <Right />
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,15 @@
+import './Login.css';
+import React from 'react';
+import Left from '../Left/Left';
+import Right from '../Right/Right';
+
+function Login() {
+  return (
+    <div className="Login">
+      <Left />
+      <Right />
+    </div>
+  );
+}
+
+export default Login;
